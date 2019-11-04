69481a3ac7ed8d5a120345eb913d07ed99f4bf47
Daksh Miglani <hello@dak.sh>
2019-11-04 23:19:39 +0530

    a better class approach
    make compile_file a protected method

--- modiscript/api.py
+++ modiscript/api.py
@@ -1,26 +1,23 @@
 from lexer import Lexer
 from parser import Parser
 
-DEBUG = False
 
-def compile_file(value, valueType="filename"):
-    global DEBUG
-    lex_out = Lexer(value, valueType).analyze()
-    if DEBUG:
-        with open(filename.split('.', 1)[0] + ".txt", "w") as f:
-            print(*lex_out, sep='\n', file=f)
-    parse_out = Parser(lex_out).parse()
-    if DEBUG:
-        with open(filename.split('.', 1)[0] + ".py", "w") as f:
-            import ast
-            print(ast.dump(parse_out), file=f)
-    return compile(parse_out, "<ast>", "exec")
+class Api:
+    def __init__(self, debug=False):
+        self.debug = debug
 
+    def _compile_file(self, value, valueType="filename"):
+        lex_out = Lexer(value, valueType).analyze()
+        if self.debug:
+            with open(filename.split('.', 1)[0] + ".txt", "w") as f:
+                print(*lex_out, sep='\n', file=f)
+        parse_out = Parser(lex_out).parse()
+        if self.debug:
+            with open(filename.split('.', 1)[0] + ".py", "w") as f:
+                import ast
+                print(ast.dump(parse_out), file=f)
+        return compile(parse_out, "<ast>", "exec")
 
-def execute(value, valueType="filename", debug=False):
-    if debug:
-        global DEBUG
-        DEBUG = debug
-    ast_module = compile_file(value, valueType)
-    exec(ast_module)
-
+    def execute(self, value, valueType="filename"):
+        ast_module = self._compile_file(value, valueType)
+        exec(ast_module)
