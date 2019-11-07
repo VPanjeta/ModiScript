9f9985fb81cc26a7b24ca285a8933e68b00bc5d5
Daksh Miglani <hello@dak.sh>
2019-11-07 14:35:09 +0530

    modiscript web ui complete

---
+++ website/ui/src/codes.js
@@ -0,0 +1,52 @@
+export default {
+  "hello-world": `Mitrooon
+ bhaiyo aur behno "Hello World"
+Achhe din aa gaye`,
+  "if-else": `Mitrooon
+agar baat sach hai
+    bhaiyo aur behno "Modi sabse achhe pradhan mantri hai."
+nahi toh
+    bhaiyo aur behno "Rahul Gandhi ko PM banaiye"
+Achhe din aa gaye
+`,
+  mandir: `mitrooon
+UP "BJP governed state hai"
+SP "opposition me hai"
+agar ye sach hai
+	bhaiyo aur behno "Mandir wahin banayenge"
+nahi toh
+	bhaiyo aur behno "Mandir to wahin banega"
+achhe din aa gaye`,
+  scam: `mitrooon
+itne 1 hai
+jab tak itne 100 se zyada{
+	bhaiyo aur behno "Congress ne " hai
+	bhaiyo aur behno itne hai
+	bhaiyo aur behno "crore ka scam kiya"
+	itne (itne+1) hai
+}
+achhe din aa gaye
+`,
+  factorial: `
+  mitroon
+  factorial 1 hai
+  i 1 hai
+  bhaiyo aur behno "ek nambar do"
+  mann ki baat nambar
+  agar nambar 0 se kam hai 
+    bhaiyo aur behno "nakaratmak sankhyao ka factorial nai hota bhaiyo aur behno par mujhe sirf 50 din dijiye"
+  nahi toh {
+    jab tak i nambar se zyada hai {
+      factorial factorial taimes i hai
+      i i plus 1 hai
+    }
+  }
+    
+  bhaiyo aur behno nambar
+  bhaiyo aur behno "ka factorial"
+  bhaiyo aur behno factorial
+  bhaiyo aur behno "hai"
+  acche din aa gaye
+`,
+  custom: ""
+};
